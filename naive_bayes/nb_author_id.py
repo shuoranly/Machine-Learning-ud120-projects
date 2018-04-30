5c76456cfd604c0f577c0c0f42e787f7f8062b21
shuoranly <shuoranly@163.com>
2018-04-30 13:45:38 +0800

    naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,8 +12,9 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("F:\Work\Machine_Learning\ud120-projects\tools")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -23,10 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
+result = clf.predict(features_test)
+print "training time:", round(time()-t0, 3), "s"
+
+label = result.tolist()
+m = 0
+for i in range(0, len(label)):
+    if label[i] == labels_test[i]:
+        m += 1
+print float(m)/len(label)
 
 #########################################################
 
